f1035913c0a76039eba2bb624f6ae57e2f516bf9
cmarigo <110262909+cmarigo@users.noreply.github.com>
2025-04-06 20:51:54 -0300

    Inserção de thread e mutex

--- maze_runner.cpp
+++ maze_runner.cpp
@@ -12,6 +12,8 @@ Julia Pereira Maia Ribeiro 2021014864
 #include <stack>
 #include <thread>
 #include <chrono>
+#include <mutex>
+#include <atomic>
 
 // Representação do labirinto
 char** maze = nullptr;
@@ -26,6 +28,9 @@ struct Position {
 int num_rows = 0;
 int num_cols = 0;
 std::stack<Position> valid_positions;
+std::atomic<bool> exit_found(false);
+std::mutex m;
+std::mutex m_print;
 
 // Função para carregar o labirinto de um arquivo
 Position load_maze(const char* file_name, char*** mazePtr, int& num_rows, int& num_cols) {
@@ -80,6 +85,7 @@ void print_maze(char** maze, int num_rows, int num_cols) {
         }
         std::cout << std::endl;
     }
+    std::cout << std::endl;
 }
 
 // Função para verificar se uma posição é válida
@@ -89,11 +95,20 @@ bool is_valid_position(int row, int col) {
 
 // Função principal para navegar pelo labirinto
 bool walk(Position pos) {
+    m.lock();
     valid_positions.push(pos); 
-
+    m.unlock();
+    
     while (!valid_positions.empty()) {
+        m.lock();
+        if (exit_found) {
+            m.unlock();
+            return false;
+        }
+        
         Position current_pos = valid_positions.top();
         valid_positions.pop();
+        m.unlock();
 
         int i = current_pos.row;
         int j = current_pos.col;
@@ -102,16 +117,23 @@ bool walk(Position pos) {
 
         // Verifica se a posição atual é a saída
         if (maze[i][j] == 's') {
+            m.lock();
             maze[i][j] = 'o'; 
+            exit_found = true;
+            m.unlock();
             print_maze(maze, num_rows, num_cols);
             return true;
         }
 
         // Marca a posição atual como visitada
         if (maze[i][j] == 'x') {
+            m.lock();
             maze[i][j] = '.';
+            m.unlock();
         } else if (maze[i][j] == 'e') {
+            m.lock();
             maze[i][j] = '.';
+            m.unlock();
         }
         // Direções: direita, esquerda, baixo, cima
         Position directions[4] = {{i, j + 1}, {i, j - 1}, {i + 1, j}, {i - 1, j}};
@@ -119,11 +141,14 @@ bool walk(Position pos) {
         for (auto& dir : directions) {
             if (is_valid_position(dir.row, dir.col)) {
                 // Adiciona a posição válida e não visitada à pilha
+                m.lock();
                 valid_positions.push(dir);
+                m.unlock();
             }
         }
-
+        m_print.lock();
         print_maze(maze, num_rows, num_cols);
+        m_print.unlock();
     }
 
     return false; 
@@ -131,9 +156,11 @@ bool walk(Position pos) {
 
 int main(int argc, char* argv[]) {
     Position initial_pos = load_maze(argv[1], &maze, num_rows, num_cols);
-    std::cout << "Posição inicial: (" << initial_pos.row << ", " << initial_pos.col << ")" << std::endl;
 
-    bool exit_found = walk(initial_pos);
+    std::thread t(walk, initial_pos);
+
+    t.join();
+
     if(exit_found) {
         std::cout << "Saída encontrada!" << std::endl;
     } else {
